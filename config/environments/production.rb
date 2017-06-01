80c825b4d17d2cb55c43a65d0f355b8922aed632
Philip10023 <philipormorgan@gmail.com>
2017-06-01 12:14:26 -0400

    security hole patched

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
 
-  config.action_mailer.default_url_options = { :host => "https://safe-retreat-29413.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://reality-checker.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
