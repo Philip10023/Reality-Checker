27100d79c487f4b5bc3282d1dbd37ed7b0a4632f
Philip10023 <philipormorgan@gmail.com>
2017-08-23 17:17:35 -0400

    fixed mailer issue

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     super
-    UserMailer.welcome_email(@user).deliver
+    if @user.email == ""
+    else
+      UserMailer.welcome_email(@user).deliver
+    end
   end
 end
